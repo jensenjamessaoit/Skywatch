bc12a02b4189afdc824706ff8eb8db2bb79b3300
jj <saoitjensen@gmail.com>
2023-08-03 19:48:41 -0600

    flight api work

--- scipt.js
+++ scipt.js
@@ -1,22 +1,27 @@
 var userNum = document.querySelector('#user_num');
 var submitNum = document.querySelector('#submit_num');
 var flightStatus = document.querySelector('#flight_status');
-const url = 'https://timetable-lookup.p.rapidapi.com/TimeTable/BOS/LAX/20230802/?Max_Results=1';
+const url = 'https://flight-info-api.p.rapidapi.com/status?version=v2&DepartureDateTime=2023-07-04&ArrivalDateTime=2023-07-04&DepartureAirport=SLC&ArrivalAirport=LAS&CodeType=IATA';
 const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'd4234982dfmsh11ff69add0c142cp1338dbjsnea33a2a2cc14',
-		'X-RapidAPI-Host': 'timetable-lookup.p.rapidapi.com'
+		'X-RapidAPI-Host': 'flight-info-api.p.rapidapi.com'
 	}
 };
 
-function getStatus(){
+function getStatus(){  
+    var flightNum = 4988;
+
     fetch(url, options)
         .then(function (response) {
-            return response.text();
+            return response.json();
         })
         .then(function(data){
+            console.log(flightNum);
             console.log(data);
+            console.log(data.data.length);
+            console.log(data.data[0].flightNumber);
         })
 }
 
